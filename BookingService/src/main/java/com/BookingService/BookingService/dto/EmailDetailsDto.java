85b85e9579de7fdec458f6ff06c741acc1763ffd
akilanilusha <akilanilusha89@gmail.com>
2026-01-05 00:53:13 +0530

    emailservice bug fix

--- BookingService/src/main/java/com/BookingService/BookingService/dto/EmailDetailsDto.java
+++ BookingService/src/main/java/com/BookingService/BookingService/dto/EmailDetailsDto.java
@@ -7,7 +7,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ConfirmBookingEmailRequest {
-    private Long bookingId;
-    private EmailDetailsDto email;
+public class EmailDetailsDto {
+    private String recipient;
+    private String msgBody;
+    private String subject;
+    private String attachment;
 }
